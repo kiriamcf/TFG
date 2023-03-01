4d8b4b1f82a7828a33941693f74d36078bf4292f
Kiriam Campobadal <kiriam@smartaos.com>
2023-03-01 18:56:27 +0100

    first commit

---
+++ lyricsExtraction.py
@@ -0,0 +1,13 @@
+from lyricsgenius import Genius
+import pandas as pd
+import string
+
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+def test():
+    genius = Genius()
+    return
+print()
