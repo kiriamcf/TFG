aaff52d49c7eb6cea939c0c1e52a7f304ecd80f6
Kiriam Campobadal <kiriam@smartaos.com>
2023-03-02 08:14:43 +0100

    initial tests

--- lyricsExtraction.py
+++ lyricsExtraction.py
@@ -1,13 +1,27 @@
+import api
 from lyricsgenius import Genius
 import pandas as pd
-import string
+# import string
 
-import nltk
-from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import stopwords
+# import nltk
+# from nltk.tokenize import word_tokenize
+# from nltk.stem import WordNetLemmatizer
+# from nltk.corpus import stopwords
 
-def test():
-    genius = Genius()
-    return
-print()
+def main():
+    genius = Genius(api.genius_access_token)
+    genius.remove_section_headers = True
+    genius.skip_non_songs = False
+
+    titles = []
+    artists = []
+    lyrics = []
+    albums = []
+    publishedYears = []
+
+    artist = genius.search_artist("Andy Shauf", max_songs=3, sort="popularity")
+    song = artist.song("To You")
+    artist.save_lyrics()
+
+if __name__ == "__main__":
+    main()
