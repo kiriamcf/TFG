89229d5f42a77326f0c54f459669ccbba205894b
Kiriam Campobadal <kiriam@smartaos.com>
2023-03-09 17:59:32 +0100

    advanced testing lyrics

--- lyricsExtraction.py
+++ lyricsExtraction.py
@@ -14,14 +14,50 @@ def main():
     genius.skip_non_songs = False
 
     titles = []
-    artists = []
     lyrics = []
-    albums = []
-    publishedYears = []
+    artists = []
+    needsClear = []
 
-    artist = genius.search_artist("Andy Shauf", max_songs=3, sort="popularity")
-    song = artist.song("To You")
-    artist.save_lyrics()
+    artist = genius.search_artist("Mac Miller", max_songs=5, sort="popularity")
+    for song in artist.songs:
+        titles.append(song.title)
+        lyrics.append(song.lyrics)
+        artists.append(song.artist)
+        if (song.lyrics.startswith("Translations")):
+            needsClear.append('Yes')
+            continue
+        needsClear.append('No')
+
+    df = pd.DataFrame({'title':titles,'artist':artists,'lyric':lyrics,'needsClear': needsClear})
+
+    df.to_csv('test.csv', index=False)
+
+    return df
+
+def testClear():
+    print('TEST CLEAR')
+    testString = "TranslationsEspañolPortuguês2009 Lyrics\r\nHola bon dia234Embed"
+
+    # Translations
+    print(testString.find('\r\n'))
+
+    # Embed
+    print(testString.find('Embed'))
+
+    while (True):
+        embedStartIndex = testString.find('Embed')
+
+        if (testString[embedStartIndex - 1].isnumeric()):
+            testString = testString[0 : embedStartIndex - 1 : ] + testString[embedStartIndex : :]
+        else:
+            break
+
+    testString = testString.replace('Embed', '')
+    print(testString)
+
+
+
 
 if __name__ == "__main__":
     main()
+    testClear()
