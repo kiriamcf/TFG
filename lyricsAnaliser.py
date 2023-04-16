e8c9584368faa796b7b8c1586da8a95be437933a
Kiriam C. Forés <kiriamcf@gmail.com>
2023-04-17 00:34:57 +0200

    documentation changes

--- lyricsAnaliser.py
+++ lyricsAnaliser.py
@@ -54,17 +54,15 @@ def lemmatize_pos_tagger(nltk_tag):
     else:
         return None
 
-def count_occurences(word_list):
-    occurance_dict = dict()
+def count_occurrences(word_list):
+    occurrence_dict = dict()
     for current_word in word_list:
-        occurance_dict[current_word] = [word_list.count(current_word)]
+        occurrence_dict[current_word] = [word_list.count(current_word)]
     # order the list in ascending order
-    occurance_dict = sorted(occurance_dict.items(), key=lambda x: x[1], reverse=True)
-    return occurance_dict
+    occurrence_dict = sorted(occurrence_dict.items(), key=lambda x: x[1], reverse=True)
+    return occurrence_dict
 
 def analyze_lyrics(lyrics):
-    # test_string = "!This is_ a tests/ for the lyrics analyser! Some other words are: churches, churchs, dogs, women, women"
-    # print('original -> ',test_string)
     # split the string into words
     lyrics_list_words = lyrics.lower().split()
     # split the words into characters
@@ -77,22 +75,30 @@ def analyze_lyrics(lyrics):
     result = nltk.pos_tag(result)
     # lemmatize the words
     result = list(map(lemmatize_words, result))
-    # remove duplicate words and indicate occurrances
-    result = count_occurences(result)
+    # remove duplicate words and indicate occurrences
+    result = count_occurrences(result)
     return result
 
 def analyse_lyrics_test():
-    test_string = "!This is_ a tests/ for the lyrics analyser! Some other words are: churches, churchs, dogs, women, woman, love, loving, loves, loved"
+    CGREEN = '\033[92m'
+    CEND = '\033[0m'
+    test_string = "String with ran!dom words like love loves loving"
+    print(CGREEN + 'original ->' + CEND , test_string)
     lyrics_list_words = test_string.lower().split()
     lyrics_list_characters = list(map(split_word, lyrics_list_words))
     result = list(map(remove_punctuations, lyrics_list_characters))
+    print(CGREEN + 'punctuations removed ->' + CEND , result)
     result = list(filter(remove_stopwords, result))
+    print(CGREEN + 'stopwords removed ->' + CEND , result)
     result = nltk.pos_tag(result)
+    print(CGREEN + 'lemmatization tags applied ->' + CEND , result)
     result = list(map(lemmatize_words, result))
-    result = count_occurences(result)
+    print(CGREEN + 'words lemmatized ->' + CEND , result)
+    result = count_occurrences(result)
+    print(CGREEN + 'word occurrences ->' + CEND , result)
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
     # analyze_lyrics()
     # analyze_lyrics(dataframe)
     # a partir del dataframe seleccionar la columna dels lyrics, i passar aixo als filtres
-    # analyse_lyrics_test()
+    analyse_lyrics_test()
